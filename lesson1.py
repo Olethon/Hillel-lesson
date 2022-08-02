f30537ef310b90dbc1f139b9a95b211fe299e368
Origin <https://github.com/Olethon/Hillel-lesson.git>
2022-08-02 18:44:08 +0300

    init

---
+++ lesson1.py
@@ -0,0 +1,7 @@
+import this                     # імпортування принципів (Філософї) Пітона
+
+import antigravity              # імпортування переходу до коміксу з ресурсу XKCD
+
+import __hello__                # імпортуввання привітання "helo world"
+
+from __future__ import braces   # Імпортування "braces" з модуля "future"
